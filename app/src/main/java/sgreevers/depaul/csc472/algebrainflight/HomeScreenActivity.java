407ee1d8abd0a72fdea0c387eec6500ef4d16d72
greevers <greevers@gmail.com>
2017-11-14 13:01:31 -0600

    Initial commit

---
+++ app/src/main/java/sgreevers/depaul/csc472/algebrainflight/HomeScreenActivity.java
@@ -0,0 +1,13 @@
+package sgreevers.depaul.csc472.algebrainflight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
